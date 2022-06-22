cfdfbcd2da8ecd7eee96ba48aabdc3304dad1d5d
Mukesh20221 <107830305+Mukesh20221@users.noreply.github.com>
2022-06-23 00:02:08 +0530

    Mukesh Verma

---
+++ BasicInformation.java
@@ -0,0 +1,8 @@
+package Day3;
+public class BasicInformation {
+	public static void main(String[] args) {
+		System.out.println("Nmae: Mukesh Verma");
+		System.out.println("Course: MSC(CS)");
+		System.out.println("Stream: Computer Science");
+	}
+}
